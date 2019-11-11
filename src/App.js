ffe3f90578b5f04b151cdee1e55eabeff739220a
anshularanjit <42152100+anshularanjit@users.noreply.github.com>
2019-11-11 14:52:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
 import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer';
+
+var __html = require('./homepage.html');
+var template = { __html: __html };
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <PathfindingVisualizer></PathfindingVisualizer>
-    </div>
+	<Router>
+		<Switch>
+			// Open the homepage
+			
+
+			<Route path="/visualizer" render={props => 
+    				(<div className="App">
+      					<PathfindingVisualizer/> 
+    				</div>) } >
+			</Route>
+	  <Route path="/">
+				<div dangerouslySetInnerHTML={template} />
+			</Route>
+		</Switch>
+	</Router>
   );
 }
 
